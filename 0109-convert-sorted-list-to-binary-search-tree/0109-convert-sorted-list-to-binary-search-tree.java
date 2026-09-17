61ac730592a9153329417fdfcd1dd780d7314ecd
arpitsaxena015 <arpitsaxenamarch1996@gmail.com>
2026-09-17 12:39:48 +0530

    Time: 0 ms (100%), Space: 46.9 MB (43.67%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
@@ -7,22 +7,22 @@ class Solution {
         ListNode slow = head;
         ListNode fast = head;
 
-        // Find the middle element using slow and fast pointers
+        
         while (fast != null && fast.next != null) {
             prev = slow;
             slow = slow.next;
             fast = fast.next.next;
         }
 
-        // Disconnect the left half from the middle node
+        
         if (prev != null) {
             prev.next = null;
         }
 
-        // Mid node becomes the root
+        
         TreeNode root = new TreeNode(slow.val);
 
-        // Recursively build left and right subtrees
+        
         root.left = sortedListToBST(head);
         root.right = sortedListToBST(slow.next);
 
